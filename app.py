5d29297d47511de588a14f11b0aa7b80f88a63d8
dbtmddn41 <dbtmddn41@gmail.com>
2024-07-29 16:27:48 +0900

    add gemini

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, render_template
 from openai import OpenAI
+import google.generativeai as genai
 from flask_socketio import SocketIO, emit
-import openai_api_key, oracle_configs
+import openai_api_key, gemini_api_key, oracle_configs
 import oracledb
 import sys
 
-
 # API 키 설정
 client = OpenAI(api_key=openai_api_key.OPENAI_API_KEY)
+genai.configure(api_key=gemini_api_key.GEMINI_API_KEY)
 
 # app
 app = Flask(__name__)
@@ -52,23 +53,32 @@ def upsert_chat_history(table_name, role, message):
 @socketio.on('connect')
 def connected():
     global messages
-    messages = [("system", "You are an assistant for elderly people with dementia. All you have to do is talk to them affectionately, like you would their children. And you must speak in Korean.")]
+    messages = []
     
-    emit('my_response', {'data': 'Connected'})
+    emit('alert', {'data': 'Connected'})
 
-@socketio.on('my_event')
-def handle_my_custom_event(obj):
+@socketio.on('user_send')
+def handle_user_msg(obj):
+    print(obj)
     upsert_chat_history('chat_table', 'user', obj['data'])
     
-    messages.append(('user', obj['data']))
-    response = get_openai_message()
+    if obj['ai_option'] == 'openai':
+        response = get_openai_message(obj['data'])
+    elif obj['ai_option'] == 'gemini':
+        response = get_gemini_message(obj['data'])
+    else:
+        response = "어쩔티비 안물안궁"
     upsert_chat_history('chat_table', 'assistant', response)
     messages.append(('assistant', response))
     
     d = {}
     d['data'] = response
 
-    emit('my_response', d)
+    emit('ai_response', d)
+
+@socketio.on('audio_data')
+def handle_user_audio(obj):
+    pass
 
 def txt2voice(txt):
     speech_file_path = f"./audio_files/created_.wav"
@@ -80,29 +90,43 @@ def txt2voice(txt):
     response.stream_to_file(speech_file_path)
     return response
 
-def apply_openai_chat_template():
-    # [
-    #         {
-    #             "role": "system",
-    #             "content": "You are an assistant for elderly people with dementia. All you have to do is talk to them affectionately, like you would their children. And you must speak in Korean."
-    #         },
-    #         {
-    #             "role": "user",
-    #             "content": msg
-    #         }
-    #     ]
+def apply_chat_template(ai_option='openai'):
+#    history=[{'parts':
+#         {
+#             "text": "You are an assistant for elderly people with dementia. All you have to do is talk to them affectionately, like you would their children. And you must speak in Korean."
+#         },
+#         "role": "user",},
+#     ]
     chat_template = []
     global messages
     for m in messages:
-        chat_template.append({'role': m[0], 'content': m[1]})
-    print(chat_template)
+        if ai_option == 'openai':
+            chat_template.append({'role': m[0], 'content': m[1]})
+        elif ai_option == 'gemini':
+            chat_template.append({'role': m[0], 'parts': {'text': m[1]}})
     return chat_template
 
-def get_openai_message():
+def get_gemini_message(msg):
     # Create a chat completion
+    model = genai.GenerativeModel('gemini-1.5-flash',
+                    system_instruction="You are an assistant for elderly people with dementia. All you have to do is talk to them affectionately, like you would their children. And you must speak in Korean.",
+                    generation_config=genai.GenerationConfig(
+                                max_output_tokens=128,
+                                temperature=0.5,
+                    ))
+    chat = model.start_chat(history=apply_chat_template('gemini'))
+    response = chat.send_message(msg)
+    return response.text
+
+
+def get_openai_message(msg):
+    # Create a chat completion
+    global messages
+    messages.insert(0, ("system", "You are an assistant for elderly people with dementia. All you have to do is talk to them affectionately, like you would their children. And you must speak in Korean."))
+    messages.append(('user', msg))
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=apply_openai_chat_template(),
+        messages=apply_chat_template('openai'),
         temperature=0.7,
         max_tokens=128,
         top_p=0.8
