ff9705bc56771023a5803cb596106a358648df35
yundh09 <yundh135@naver.com>
2024-08-06 15:14:24 +0900

    .

---
+++ email.config.py
@@ -0,0 +1,4 @@
+EMAIL_ADDRESS = 'yundh0925@gmail.com'  # 발신자 이메일 주소
+EMAIL_PASSWORD = 'yundh13579^^'  # 발신자 이메일 비밀번호
+SMTP_SERVER = 'smtp.gmail.com'  # Gmail SMTP 서버 주소
+SMTP_PORT = 587  # Gmail SMTP 서버 포트
