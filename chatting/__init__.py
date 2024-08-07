3374f0acbd9968fab534f6eb002306ddaed2e58f
dbtmddn41 <dbtmddn41@gmail.com>
2024-08-07 11:44:37 +0900

    채팅방 재입장 시에도 과거 메세지가 챗봇에 전달되도록 수정

--- chatting/__init__.py
+++ chatting/__init__.py
@@ -21,7 +21,6 @@ def create_app():
     from . import models
     
     from .views import main_views, chatting_views, auth_views
-    from .views.utils import vector_search
     app.register_blueprint(main_views.bp)
     app.register_blueprint(chatting_views.bp)
     app.register_blueprint(auth_views.bp)
